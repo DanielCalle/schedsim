e2ebde30d5b2f120c97b1e398c37be617320e032
Daniel Calle <danielcallesanchez@gmail.com>
2017-04-24 12:05:57 +0200

    Codigo listo para completar

--- sched_prio.c
+++ sched_prio.c
@@ -1,6 +1,6 @@
 #include "sched.h"
 
-static task_t* pick_next_task_fcfs(runqueue_t* rq,int cpu)
+static task_t* pick_next_task_prio(runqueue_t* rq,int cpu)
 {
   task_t* t=head_slist(&rq->tasks);
 
@@ -15,7 +15,7 @@ static task_t* pick_next_task_fcfs(runqueue_t* rq,int cpu)
 	return t;
 }
 
-static void enqueue_task_fcfs(task_t* t,int cpu, int runnable)
+static void enqueue_task_prio(task_t* t,int cpu, int runnable)
 {
   runqueue_t* rq=get_runqueue_cpu(cpu);
 
@@ -26,7 +26,7 @@ static void enqueue_task_fcfs(task_t* t,int cpu, int runnable)
 
 }
 
-static task_t* steal_task_fcfs(runqueue_t* rq,int cpu)
+static task_t* steal_task_prio(runqueue_t* rq,int cpu)
 {
   task_t* t=tail_slist(&rq->tasks);
 
@@ -40,8 +40,15 @@ static task_t* steal_task_fcfs(runqueue_t* rq,int cpu)
   return t;
 }
 
+static int compare_tasks_cpu_xxx(void *t1,void *t2)
+{
+	task_t* tsk1=(task_t*)t1;
+	task_t* tsk2=(task_t*)t2;
+	return   /* Complete here  */
+}
+
 sched_class_t sjf_sched= {
-  .pick_next_task=pick_next_task_fcfs,
-  .enqueue_task=enqueue_task_fcfs,
-  .steal_task=steal_task_fcfs
+  .pick_next_task=pick_next_task_prio,
+  .enqueue_task=enqueue_task_prio,
+  .steal_task=steal_task_prio
 };
