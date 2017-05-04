ace246de5748022f8850aba2f2fe67c2ce56837d
Daniel Calle <danielcallesanchez@gmail.com>
2017-05-04 23:00:40 +0200

    FCFS de Manu

--- sched_fcfs.c
+++ sched_fcfs.c
@@ -1,53 +1,64 @@
 #include "sched.h"
 
+/*
+pick_next_task() se invocará cuando se deba escoger una nueva tarea para ejecutar
+en la CPU. Seleccionará una tarea de la run queue, la eliminará de la run queue y
+devolverá dicha tarea. Es la función que realmente lleva a cabo la política de
+planificación deseada.
+*/
 static task_t* pick_next_task_fcfs(runqueue_t* rq,int cpu)
 {
-  task_t* t=head_slist(&rq->tasks);
 
-	/* Current is not on the rq -> let's remove it */
-	if (t) {
-		remove_slist(&rq->tasks,t);
+	// Obtenemos la tarea de la primera que esta para ser procesada en la lista.
+	// Toda run_queue tiene una task_list.
+	task_t* t = head_slist(&rq->tasks);
 
-    /* Complete here  */
-
-  }
+	// Si hay una tarea encolada, la sacamos de la run_queue y la devolvemos.
+	if (t) remove_slist(&rq->tasks,t);
 
 	return t;
+
 }
 
+/*
+enqueue_task() se invocará cada vez que debamos encolar una tarea en la run queue
+de una CPU, bien porque acaba de crearse, porque sale de un bloqueo o porque se ha
+migrado desde otra run queue.
+*/
 static void enqueue_task_fcfs(task_t* t,int cpu, int runnable)
 {
-  runqueue_t* rq=get_runqueue_cpu(cpu);
 
-	if (t->on_rq || is_idle_task(t))
-		return;
+	// Obtenemos la runqueue de una determinada CPU.
+	runqueue_t* rq = get_runqueue_cpu(cpu);
 
-  /* Complete here  */
+	// Cada run queue tiene su propia idle_task que tomará la CPU cuando no haya
+	// ninguna tarea runnable, esto es, en disposición de ejecutarse.
+
+	// Si la tarea ya esta en la run_queue o no hay tareas en disposición
+	// de ejecutarse acabamos.
+	if(t->on_rq || is_idle_task(t)) return;
+
+	// Si la tarea no esta encolada en la run_queue de la cpu especificada,
+	// encolamos dicha tarea.
+	insert_slist(rq->tasks,t);
 
 }
 
+/*
+steal_task() se invocará cuando se lleve a cabo una migración que exija robar una
+tarea de la run queue rq. La tarea escogida se sacará de la run queue y se devolverá como
+argumento de salida de la función.
+ */
 static task_t* steal_task_fcfs(runqueue_t* rq,int cpu)
 {
-  task_t* t=tail_slist(&rq->tasks);
 
-  if (t) {
-    remove_slist(&rq->tasks,t);
+	// Devuelve el último elemento de la lista (pero no lo saca de ella).
+	task_t* t = tail_slist(&rq->tasks);
 
-    /* Complete here  */
+	// Removemos de la run_queue la tarea especificada.
+	if(t) remove_slist(&rq->tasks,t);
 
-  }
-
-  return t;
-}
-
-static void task_tick_fcfs(runqueue_t* rq,int cpu)
-{
-	task_t* current=rq->cur_task;
-
-	if (is_idle_task(current))
-		return;
-
-  /* Complete here */
+	return t;
 
 }
 
@@ -55,5 +66,4 @@ sched_class_t sjf_sched= {
   .pick_next_task=pick_next_task_fcfs,
   .enqueue_task=enqueue_task_fcfs,
   .steal_task=steal_task_fcfs
-  .task_tick=task_tick_fcfs,
 };
